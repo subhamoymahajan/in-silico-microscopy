1e04cd00c281f40d23da49fb4d23264e3a938de5
Subhamoy Mahajan <subhamoy@cedar5.cedar.computecanada.ca>
2020-12-22 23:06:51 -0800

    more corrections

--- Tutorial/Tut1/run_genpsf.py
+++ Tutorial/Tut1/run_genpsf.py
@@ -1,8 +1,10 @@
 import sys
 sys.path.insert(1,'../../')
 import gen_psf as gpsf
+import numpy as np
 NA=1.3
 meu=1.51
+beta=np.arcsin(NA/meu)
 lambd=518
 dx=0.1
 dy=0.1
@@ -11,8 +13,6 @@ Lx=15
 Ly=15
 Lz=50
 fs=800
-#gpsf.psf_gandy(NA,meu,lambd,dx,dy,dz,Lx,Ly,Lz,fs,'PSF_gandy_lam'+str(lambd)+'_fs'+str(fs)+'.dat')
+gpsf.psf_gandy(beta,lambd,dx,dy,dz,Lx,Ly,Lz,fs,'PSF_gandy_lam'+str(lambd)+'_fs'+str(fs)+'.dat')
 lambd=670
-#gpsf.psf_gandy(NA,meu,lambd,dx,dy,dz,Lx,Ly,Lz,fs,'PSF_gandy_lam'+str(lambd)+'_fs'+str(fs)+'.dat')
-for k in range(83,126):
-    gpsf.psf_gandy_sep(NA,meu,lambd,dx,dy,dz,Lx,Ly,Lz,fs,'PSF_gandy_lam'+str(lambd)+'_fs'+str(fs)+'.dat','a',k)
+gpsf.psf_gandy(beta,lambd,dx,dy,dz,Lx,Ly,Lz,fs,'PSF_gandy_lam'+str(lambd)+'_fs'+str(fs)+'.dat')
--- gen_psf.py
+++ gen_psf.py
@@ -15,41 +15,38 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.)
 
-# Generates point spread function for a in-silico microscope
+# This code generates a point spread function for a in-silico microscope at 3D grid points
 
 import scipy.integrate as integrate
 import scipy.special as special
 import numpy as np
 
-def psf_gandm(NA,meu,lambd,dl,dm,dn,Ll,Lm,Ln,fs,outname):
+def psf_gandy(beta,lambd,dl,dm,dn,Ll,Lm,Ln,fs,outname):
    Nn=int((int(Ln/dn)+1)/2)+1
    for k in range(Nn):
-       psf_gandm_sep(NA,meu,lambd,dl,dm,dn,Ll,Lm,Ln,fs,outname,'a',k)       
+       psf_gandy_sep(beta,lambd,dl,dm,dn,Ll,Lm,Ln,fs,outname,'a',k)       
 
-def psf_gandm_sep(NA,meu,lambd,dl,dm,dn,Ll,Lm,Ln,fs,outname,otype,zidx):
-# Based on  R O Gandm 1954 Proc. Phys. Soc. B 67 825
-# NA = numerical aperture
-# meu = refractive index of the immersion oil
-# lambd = wavelength of light
-# dl, dm pixel dimensions. dn is the distance in the sectioned object plane (gro file)
-# Ln, Lm, Ln is the dimension of the psf box
-# fs = FWHM scaling factor; scaling factor of wavenumber (2pi/lambda); scaling factor of "gro" coordinates. (New Paper Cite) 
+def psf_gandy_sep(beta,lambd,dl,dm,dn,Ll,Lm,Ln,fs,outname,otype,nidx):
+# Based on  R O Gandy 1954 Proc. Phys. Soc. B 67 825-831
+# NA = Numerical aperture
+# meu = Refractive index of the immersion oil
+# lambd = Wavelength of light
+# dl, dm, and dn = Distance between consecutive grid points 
+# Ln, Lm, and Ln = Dimensions of the PSF box
+# fs = full-width-at-half-maximum (FWHM) scaling factor; scaling factor of wavenumber (k=2pi/lambda); scaling factor of "gro" coordinates. (New Paper Cite) 
 # outname = the output file name
-# otype = open a file outname with type type ('a' or  'w')
-# zidx = a counter for z coordinate
+# otype = open a file outname to append or write ('a' or  'w')
+# nidx = a counter for n coordinate;  int(n/dn)
    def integrand_real(x,r,d):
         a=np.sin(x)
-        ans=a*(1-a*a)**0.25
-        ans=ans*np.cos(d*a)*special.jv(0,r*a)
-        return ans
+        b=np.cos(x)
+        return a*(b**0.5)*np.cos(d*b)*special.jv(0,r*a)
     
    def integrand_img(x,r,d):
         a=np.sin(x)
-        ans=a*(1-a*a)**0.25
-        ans=ans*np.sin(d*a)*special.jv(0,r*a)
-        return ans
+        b=np.cos(x)
+        return a*(b**0.5)*np.sin(d*b)*special.jv(0,r*a)
     
-   beta=np.arcsin(NA/meu)
    A=3/(2*(1-(np.cos(beta)**1.5)))
    K=2*np.pi*fs/lambd 
    
@@ -58,19 +55,19 @@ def psf_gandm_sep(NA,meu,lambd,dl,dm,dn,Ll,Lm,Ln,fs,outname,otype,zidx):
        w.write('# NA= '+str(NA)+' meu= '+str(meu)+' lambda= '+str(lambd)+' dl= '+str(dl)+' dm= '+str(dm)+' dn= '+str(dn)+' Ll= '+str(Ll)+' Lm= '+str(Lm)+' Ln= '+str(Ln)+' fs= '+str(fs)+'\n')
    Nl=int((int(Ll/dl)+1)/2)+1
    Nm=int((int(Lm/dm)+1)/2)+1
-   z=round(zidx*dn,6)
-   Kz=K*z
+   n=round(nidx*dn,6)
+   Kn=K*n
    for i in range(Nl):
-       x=round(i*dl,6)
+       l=round(i*dl,6)
        for j in range(i+1):
-           y=round(j*dm,6)
-           r=np.sqrt(x*x+y*y)
+           m=round(j*dm,6)
+           r=np.sqrt(l*l+m*m)
            Kr=K*r
-           H1=integrate.quad(integrand_real,0,beta,args=(Kr,Kz))
-           H2=integrate.quad(integrand_img,0,beta,args=(Kr,Kz))
+           H1=integrate.quad(integrand_real,0,beta,args=(Kr,Kn))
+           H2=integrate.quad(integrand_img,0,beta,args=(Kr,Kn))
            I=round((H1[0]*H1[0]+H2[0]*H2[0])*A*A,6)
-           w.write(str(x).rjust(10)+str(y).rjust(10)+str(z).rjust(10)+str(I).rjust(10)+'\n')
-   print('z = '+str(z)+' done')
+           w.write(str(l).rjust(10)+str(m).rjust(10)+str(n).rjust(10)+str(I).rjust(10)+'\n')
+   print('n = '+str(n)+' done')
 
 #Ref: https://www.micro-shop.zeiss.com/en/us/shop/objectives/420493-9900-000/Objective-EC-Plan-Neofluar-100x-1.3-Oil-Pol-M27#/
 #Lens: Plan-Neofluar 100x/1.3
