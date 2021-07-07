73a124512745a13aae93441f9b56312a200e71c5
Subhamoy Mahajan <subhamoymahajan@gmail.com>
2021-07-06 21:48:03 -0700

    Updated tut6

--- Tutorial/Tut6/gen_Pn.sh
+++ Tutorial/Tut6/gen_Pn.sh
@@ -4,5 +4,6 @@ for PN in 1 5 25
 do
      sed "s/Plmn\s*=.*/Plmn = 15\.0 15\.0 ${PN}\.0/g" parameters.dat > foo.dat 
      siliscopy gen_mono --file dp100.gro --paramfile foo.dat --psf PSF_gandy \
-                        --output Pn${PN}_img100
+                        --output Pn${PN}_img100 --method slice
 done
+rm foo.dat
--- Tutorial/Tut6/gen_Pn_jpeg.sh
+++ Tutorial/Tut6/gen_Pn_jpeg.sh
@@ -3,5 +3,5 @@
 for PN in 1 5 25
 do
       siliscopy plot --file Pn${PN}_img --paramfile parameters.dat \
-                     --method color --timestep 100 --calc specific 
+                     --method color --timestep 100 --calc specific --type jpeg 
 done
